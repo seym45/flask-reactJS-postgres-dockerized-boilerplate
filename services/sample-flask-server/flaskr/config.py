d09b5e9f1f2d3d107e62be51319d94ca48e2a074
seym45 <seym45@gmail.com>
2020-10-31 18:51:05 +0600

    Format to comply with pep8

--- services/sample-flask-server/flaskr/config.py
+++ services/sample-flask-server/flaskr/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-
 class Development(object):
     """
     Development environment configuration
--- services/sample-flask-server/flaskr/db/__init__.py
+++ services/sample-flask-server/flaskr/db/__init__.py
@@ -4,6 +4,3 @@ from flask_bcrypt import Bcrypt
 # initialize our db
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-
-from flaskr.models.BlogpostModel import BlogpostModel, BlogpostSchema
-from flaskr.models.UserModel import UserModel, UserSchema
